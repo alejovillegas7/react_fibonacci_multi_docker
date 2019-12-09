0c901cc36904d639d9f6017c5211021752c43c5b
alejovillegas7 <alejovillegaslopez@gmail.com>
2019-12-09 17:06:25 -0500

    changed travis yml and App test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Fib />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
