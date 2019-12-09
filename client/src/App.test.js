fe29ac1cd6b98e34af2592e30d3ce799c0321d85
alejovillegas7 <alejovillegaslopez@gmail.com>
2019-12-09 17:49:49 -0500

    installed test packages

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
-it('renders without crashing', () => {});
+it('renders the expected content', () => {});
